2258d282ca2bf63f33ce983413ca015945118558
Natalie Evans <NatalieRose93019@gmail.com>
2022-06-01 13:45:56 -0500

    Initial commit for humans vs goblins

---
+++ HumansVsGoblins/src/java/humanoid/Humanoid.java
@@ -0,0 +1,12 @@
+package humanoid;
+
+public class Humanoid {
+    static int health;
+    static int strength;
+    static int agility;
+    static int endurance;
+
+    public static void main(String[] arg) {
+
+    }
+}
